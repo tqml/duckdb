62c4490009460559616b90acb202aa260299dce3
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2024-09-20 15:37:04 +0200

    commit latest changes

--- tools/juliapkg/src/scalar_function.jl
+++ tools/juliapkg/src/scalar_function.jl
@@ -21,45 +21,88 @@ function _wrap_scalar_function(func, return_type, parameter_types)
 end
 
 
+"""
+    @_create_duckdb_scalar_function_wrapper(parameter_types, return_type)
 
+Create a wrapper function for a scalar function.
+
+# Example
+```jldoctest
+@_create_duckdb_scalar_function_wrapper([Int64, Int64], Int64)
+
+@_create_duckdb_scalar_function_wrapper((Int64, Int64), Int64)
+```
+"""
 macro _create_duckdb_scalar_function_wrapper(parameter_types, return_type)
+
+    if parameter_types.head ∉ (:tuple, :vect)
+        throw(ArgumentError("parameter_types must be a tuple or a vector"))
+    end
+
     local vector_elements = esc.(parameter_types.args)
     local data_access_var = [Symbol("data$(i)") for i in 1:length(vector_elements)]
     local data_access_assign_expr =
         [:($(data_access_var[i]) = get_array(data_chunk, $i, $(vector_elements[i]))) for i in 1:length(vector_elements)]
+
     local true_return_type = esc(return_type)
 
     return quote
+        # Create logical types for the parameters
+        local logical_parameter_types = Vector{LogicalType}()
+        for parameter_type in $(esc(parameter_types))
+            Base.push!(logical_parameter_types, create_logical_type(parameter_type))
+        end
+
+        # Get the conversion functions for the parameters
+        local loop_conversion_functions = get_conversion_loop_function.(logical_parameter_types)
+
         (info::duckdb_function_info, chunk::duckdb_data_chunk, output::duckdb_vector) -> begin
-            println("Start calling the wrapper function")
             scalar_function::ScalarFunction = unsafe_pointer_to_objref(duckdb_scalar_function_get_extra_info(info))
             data_chunk = DataChunk(chunk, false) # create a data chunk object, that does not own the data
             data_chunk_size = get_size(data_chunk) # get the size of the columns in the data chunk
+            println("Get output vec")
             output_vec = Vec(output)
-
+            println("output vec done")
             param_types::Vector{LogicalType} = scalar_function.parameters
             logical_return_type::LogicalType = scalar_function.return_type
             n_cols = get_column_count(data_chunk)
-            result_array = get_array(output_vec, $(true_return_type))
+            #result_array = get_array(output_vec, $(true_return_type))
 
             $(data_access_assign_expr...)
 
             try
+                println("start loop")
                 for i in 1:data_chunk_size
+                    # Call the scalar function with all the elements of the data chunk
+                    println("calculate result")
+                    @info "Scalar Func" scalar_function.func
                     result::$true_return_type =
                         scalar_function.func($([:($(data_access_var[j])[i]) for j in 1:length(vector_elements)]...))
-                    result_array[i] = result
+                    println("result done; write result")
+                    _duckdb_scalar_write_result(output_vec, result, i)
+                    #result_array[i] = result
+                    println("write result done")
                 end
+                println("loop done")
             catch
                 return duckdb_function_set_error(info, "ScalarFuncExc - " * get_exception_info())
             end
             return nothing
         end
     end
-
 end
 
+@inline function _duckdb_scalar_write_result(vec::Vec, result::T, i) where {T <: Union{Integer, Float32, Float64}}
+    result_array = get_array(vec, T)
+    result_array[i] = result
+    return
+end
 
+@inline function _duckdb_scalar_write_result(vec::Vec, result::AbstractString, i)
+    @info "Write string result" result i
+    #return duckdb_vector_assign_string_element_len(vector.handle, index, str, sizeof(str))
+    return assign_string_element(vec, i, result)
+end
 
 # typedef void (*duckdb_scalar_function_t)(duckdb_function_info info, duckdb_data_chunk input, duckdb_vector output);
 function _scalar_function(info::duckdb_function_info, chunk::duckdb_data_chunk, output::duckdb_vector)
@@ -106,8 +149,6 @@ function _scalar_function(info::duckdb_function_info, chunk::duckdb_data_chunk, 
             result = scalar_function.func(d[i])
             #result = scalar_function.func(inputs...)
             result_array[i] = result
-            #@info "inputs" inputs result
-
         end
 
     catch
@@ -137,25 +178,34 @@ end
 mutable struct ScalarFunction
     handle::duckdb_scalar_function
     name::AbstractString
-    parameters::Vector{LogicalType}
-    return_type::LogicalType
+    parameters::Vector{DataType}
+    return_type::DataType
+    internal_parameters::Vector{LogicalType}
+    internal_return_type::LogicalType
     func::Function
     wrapper::Union{Nothing, Function} # the wrapper function to hold a reference to it to prevent GC
 
     function ScalarFunction(
         name::AbstractString,
-        parameters::Vector{LogicalType},
-        return_type::LogicalType,
+        parameters::Vector{DataType},
+        return_type::DataType,
         func::Function,
         wrapper::Union{Nothing, Function} = nothing
     )
         handle = duckdb_create_scalar_function()
         duckdb_scalar_function_set_name(handle, name)
-        for param in parameters
+
+        logical_parameters = Vector{LogicalType}()
+        for parameter_type in parameters
+            push!(logical_parameters, create_logical_type(parameter_type))
+        end
+        logical_return_type = create_logical_type(return_type)
+
+        for param in logical_parameters
             duckdb_scalar_function_add_parameter(handle, param.handle)
         end
-        duckdb_scalar_function_set_return_type(handle, return_type.handle)
-        result = new(handle, name, parameters, return_type, func, wrapper)
+        duckdb_scalar_function_set_return_type(handle, logical_return_type.handle)
+        result = new(handle, name, parameters, return_type, logical_parameters, logical_return_type, func, wrapper)
         finalizer(_destroy_scalar_function, result)
         duckdb_scalar_function_set_extra_info(handle, pointer_from_objref(result))
 
@@ -207,14 +257,14 @@ macro create_scalar_function(name, parameters, return_type, func)
     func_pointer = :(@cfunction($func_wrapper, Core.Cvoid, (duckdb_function_info, duckdb_data_chunk, duckdb_vector)))
     # create the scalar function
     quote
+        local func_wrapper = @_create_duckdb_scalar_function_wrapper($parameters, $return_type)
+        # local parameter_types = Vector{LogicalType}()
+        # for parameter_type in $(esc(parameters))
+        #     Base.push!(parameter_types, create_logical_type(parameter_type))
+        # end
 
-        #local func_wrapper = @_create_duckdb_scalar_function_wrapper($parameters, $return_type)
-        local parameter_types = Vector{LogicalType}()
-        for parameter_type in $(esc(parameters))
-            Base.push!(parameter_types, create_logical_type(parameter_type))
-        end
-
-        fun = ScalarFunction($name, parameter_types, create_logical_type($return_type), $(esc(func)), $func_wrapper) # do not use generic wrapper
+        #fun = ScalarFunction($name, parameter_types, create_logical_type($return_type), $(esc(func)), $func_wrapper) # do not use generic wrapper
+        fun = ScalarFunction($name, $parameters, $return_type, $(esc(func)), $func_wrapper) # do not use generic wrapper
         duckdb_scalar_function_set_function(fun.handle, $func_pointer)
         fun
     end
@@ -253,3 +303,8 @@ end
 
 unregister_scalar_function(db::DB, fun::ScalarFunction) = unregister_scalar_function(db.main_connection, fun)
 unregister_scalar_function(db::DB, name::AbstractString) = unregister_scalar_function(db.main_connection, name)
+
+
+
+
+
